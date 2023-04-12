b5cb90b34ddcd5c6fc8ebe8d8668c3ddfbbd75de
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-12 17:36:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    uvicorn.run(app, host="0.0.0.0", port=5000)
     bot.run()
