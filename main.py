d3c9cad8a49844e9b04cdc5ffa599aedfd0bbe45
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-12 14:52:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #from config import bot
 from pyrogram import filters,idle,Client
+from fastapi import FastAPI
+
 
 ID = os.environ.get("ID")
 HASH = os.environ.get("HASH")
@@ -10,6 +12,12 @@ bot = Client("Asta-Robot-Vercel",
              api_hash=HASH,
              bot_token=TOKEN,
              in_memory=True)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "ASTA-ROBOT RUN!"}
 
 
 @bot.on_message(filters.command('start'))
