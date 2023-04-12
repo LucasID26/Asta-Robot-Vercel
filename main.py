d120ec55de437b328336cb8181cdf3c4875f009d
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-12 14:12:10 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from config import bot
+from pyrogram import filters,idle
+
+
+@bot.on_message(filters.command('start'))
+async def start(client,m):
+    await m.reply_text("Start")
+
+
+bot.start()
+idle()
+bot.stop()
