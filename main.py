66afec25496140e43753923b94808f3ff482d264
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-12 14:42:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from config import bot
-from pyrogram import filters,idle
+#from config import bot
+from pyrogram import filters,idle,Client
+
+ID = os.environ.get("ID")
+HASH = os.environ.get("HASH")
+TOKEN = os.environ.get("TOKEN")
+
+bot = Client("Asta-Robot-Vercel", 
+             api_id=ID,
+             api_hash=HASH,
+             bot_token=TOKEN,
+             in_memory=True)
 
 
 @bot.on_message(filters.command('start'))
@@ -7,4 +17,4 @@ async def start(client,m):
     await m.reply_text("Start")
 
 
-bot.start()
+bot.run()
