032d03b3aa7fad9ac830396e3fa6ac51222e4dd4
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-13 22:54:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,37 @@ from pyrogram import Client, filters,idle
 from fastapi import FastAPI
 import uvicorn
 
-bot = Client(
-    "ASTA-ROBOT-VERCEL",
-    api_id=int(os.environ.get("ID")),
-    api_hash=os.environ.get("HASH"),
-    bot_token=os.environ.get("TOKEN")
-)
 
-app = FastAPI()
+with open("uffizzi.yml", "r") as f:
+    config = yaml.safe_load(f)["pyrogram"]
+
+# Buat instance Pyrogram Client
+bot = Client(**config)
+
+#bot = Client(
+   # "ASTA-ROBOT-VERCEL",
+    #api_id=int(os.environ.get("ID")),
+   # api_hash=os.environ.get("HASH"),
+  #  bot_token=os.environ.get("TOKEN"))
+
+#app = FastAPI()
 
 @bot.on_message(filters.command("start"))
 async def start(client, m):
     await m.reply_text("Halo! Bot Pyrogram sudah berjalan di Vercel.")
 
+
+bot.run()
 # Route untuk halaman utama website
-@app.get("/")
-def read_root():
-    return {"Hello": "world"}
+#@app.get("/")
+#def read_root():
+  #  return {"Hello": "world"}
 
-def run_bot():
-    bot.start()
-    idle()
-    bot.stop()
+#def run_bot():
+   # bot.start()
+    #idle()
+   # bot.stop()
 
-if __name__ == "__main__":
-    Thread(target=run_bot).start()
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#if __name__ == "__main__":
+  #  Thread(target=run_bot).start()
+   # uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
